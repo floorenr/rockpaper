f73897c509f92a90415e7e3a5385691fece19e60
Rene Flooren <floorenr@gmail.com>
2021-03-13 15:05:16 +0100

    Created function startGame, prompting user for a userSelection, make it lowercase, invoke function computerPlay, and test both variables, to return either incorrect input, a tie, winning or losing the game

--- rockpaper.js
+++ rockpaper.js
@@ -1,8 +1,28 @@
 // 0 = rock, 1 = paper, 2, scissors 
-let computerSelection
+let computerSelection;
 function computerPlay () {
     computerSelection = Math.floor(Math.random() * 3);
     if (computerSelection === 0) {computerSelection = 'rock';}
     else if (computerSelection === 1) {computerSelection = 'paper';}
     else {computerSelection = 'scissors';}
+}
+function startGame () {
+    let userSelection = prompt('Rock, paper or scissors?');
+    userSelection = userSelection.toLowerCase();
+    computerPlay ();
+    if (!['rock','paper','scissors'].includes(userSelection)) {
+        return ('incorrect input');
+    }
+    else if (userSelection === computerSelection) {
+        return ('It\'s a tie, you both picked ' + userSelection + '.');
+    }
+    else if ((userSelection === 'rock' && computerSelection === 'paper') || 
+             (userSelection === 'paper' && computerSelection === 'scissors') ||
+             (userSelection === 'scissors' && computerSelection === 'rock'))
+         {
+        return ('You lost! Computer\'s ' + computerSelection + ' beats your ' + userSelection + '.')
+    }
+    else { return ('You won! Your ' + userSelection + ' beats computer\'s ' + computerSelection + '.')
+    }
+    
 }
