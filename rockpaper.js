202d513e194136ff4d37861d0bbe1e00e90b8013
Rene Flooren <floorenr@gmail.com>
2021-03-14 13:21:28 +0100

    Add incomplete function game() to start a sequence of 5 playRound()'s. Bugfix on playRound function to alert 'incorrect input' and loop back to the start of playRound

--- rockpaper.js
+++ rockpaper.js
@@ -1,17 +1,18 @@
 // 0 = rock, 1 = paper, 2, scissors 
 let computerSelection;
-function computerPlay () {
+function computerPlay() {
     computerSelection = Math.floor(Math.random() * 3);
     if (computerSelection === 0) {computerSelection = 'rock';}
     else if (computerSelection === 1) {computerSelection = 'paper';}
     else {computerSelection = 'scissors';}
 }
-function playRound () {
+function playRound() {
     let userSelection = prompt('Rock, paper or scissors?');
     userSelection = userSelection.toLowerCase();
     computerPlay ();
     if (!['rock','paper','scissors'].includes(userSelection)) {
-        return ('incorrect input');
+        alert ('incorrect input');
+        return playRound ();
     }
     else if (userSelection === computerSelection) {
         return ('It\'s a tie, you both picked ' + userSelection + '.');
@@ -24,5 +25,12 @@ function playRound () {
     }
     else { return ('You won! Your ' + userSelection + ' beats computer\'s ' + computerSelection + '.')
     }
-    
+}
+function game() {
+    alert('We\ll play 5 rounds of rock, paper scissors')
+    let winCount = 0;
+    let loseCount = 0;
+    for (i = 0; i < 5; i++) {
+        playRound;
+      }
 }
