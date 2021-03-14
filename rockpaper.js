49a83cc2fc02e6a25cc2853a783c3d1dec526788
Rene Flooren <floorenr@gmail.com>
2021-03-14 13:50:17 +0100

    Add global var's winCount and loseCount. Loop to console.log output of playRound five times and console.log winCount and loseCount

--- rockpaper.js
+++ rockpaper.js
@@ -1,5 +1,7 @@
 // 0 = rock, 1 = paper, 2, scissors 
 let computerSelection;
+let winCount = 0;
+let loseCount = 0;
 function computerPlay() {
     computerSelection = Math.floor(Math.random() * 3);
     if (computerSelection === 0) {computerSelection = 'rock';}
@@ -19,18 +21,18 @@ function playRound() {
     }
     else if ((userSelection === 'rock' && computerSelection === 'paper') || 
              (userSelection === 'paper' && computerSelection === 'scissors') ||
-             (userSelection === 'scissors' && computerSelection === 'rock'))
-         {
-        return ('You lost! Computer\'s ' + computerSelection + ' beats your ' + userSelection + '.')
+             (userSelection === 'scissors' && computerSelection === 'rock')) {
+                loseCount = loseCount + 1; 
+                return ('You lost! Computer\'s ' + computerSelection + ' beats your ' + userSelection + '.')
     }
-    else { return ('You won! Your ' + userSelection + ' beats computer\'s ' + computerSelection + '.')
+    else { winCount = winCount + 1;
+        return ('You won! Your ' + userSelection + ' beats computer\'s ' + computerSelection + '.')
     }
 }
 function game() {
-    alert('We\ll play 5 rounds of rock, paper scissors')
-    let winCount = 0;
-    let loseCount = 0;
+    alert('We\'ll play 5 rounds of rock, paper scissors')
     for (i = 0; i < 5; i++) {
-        playRound;
+        console.log(playRound ());
       }
+    console.log(winCount, loseCount)
 }
